1a14d3c6985c52ad86e248b31c468c6f21737e7f
victai <b04902105@ntu.edu.tw>
2018-06-21 21:23:30 +0800

    add final code

---
+++ Final/pytorch_Realtime_Multi-Person_Pose_Estimation/model/get_model.sh
@@ -0,0 +1,5 @@
+# pose_model.pth: model provided in https://github.com/tensorboy/pytorch_Realtime_Multi-Person_Pose_Estimation
+# openpose_coco_best.pth: model I trained
+
+wget https://www.dropbox.com/s/q7w9hr2foz1cc30/pose_model.pth?dl=1
+wget https://www.dropbox.com/s/chrihsrpv5a6k9o/openpose_coco_best.pth?dl=1
